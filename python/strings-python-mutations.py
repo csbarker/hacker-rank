897eb7b5110ede04718447b1a0c9f32dcd40a758
Callum Barker <callum.scott.barker@gmail.com>
2017-08-30 22:40:45 +0800

    strings-python-mutations.py

---
+++ python/strings-python-mutations.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-mutations
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    return ''.join(l)
+	
+if __name__ == '__main__':
+    s = raw_input()
+    i, c = raw_input().split()
+    s_new = mutate_string(s, int(i), c)
+    print s_new
