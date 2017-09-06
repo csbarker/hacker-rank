6e95976e8223fbbaaf62222af086b2aa6f61307f
Callum Barker <callum.scott.barker@gmail.com>
2017-09-06 23:18:16 +0800

    finding-the-percentage.py

---
+++ python/finding-the-percentage.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+if __name__ == '__main__':
+    n = int(raw_input())
+    mydict = {}
+    for line in range(n):
+        info = raw_input().split(" ")
+        score = map(float, info[1:])
+        mydict[info[0]] = sum(score) / float(len(score))
+
+    print "%.2f" % round(mydict[raw_input()],2) 
